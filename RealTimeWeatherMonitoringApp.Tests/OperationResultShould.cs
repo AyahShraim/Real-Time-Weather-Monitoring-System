693b3930508fb2ab1d0888d8d2d7b49ad7fbf79c
Ayah Shraim <ayahshraim558@gmail.com>
2023-11-08 02:13:41 +0200

    add operation result unit testing

---
+++ RealTimeWeatherMonitoringApp.Tests/OperationResultShould.cs
@@ -0,0 +1,47 @@
+﻿using AutoFixture.Xunit2;
+using RealTimeWeatherMonitoringApp.Utilities.ResultHnadler;
+
+namespace RealTimeWeatherMonitoringApp.Tests
+{
+    public class OperationResultShould
+    {
+
+        [Theory]
+        [AutoData]
+        public void CreateSuccessMessageOperationResult(string msg)
+        {
+            var result = OperationResult.SuccessResult(msg);
+            Assert.True(result.IsSuccess); 
+            Assert.Equal(msg, result.Message);     
+        }
+
+        [Theory]
+        [AutoData]
+        public void CreateFailureMessageOperationResult(string msg)
+        {
+            var result = OperationResult.FailureResult(msg);
+            Assert.False(result.IsSuccess);
+            Assert.Equal(msg, result.Message);
+        }
+
+        [Theory]
+        [AutoData]
+        public void CreateSuccessDataOperationResult(string msg, object data)
+        {
+            var result = OperationResult.SuccessDataMessage(msg, data);
+            Assert.True(result.IsSuccess);
+            Assert.Equal(msg, result.Message);
+            Assert.Equal(data, result.Data);
+        }
+
+        [Theory]
+        [AutoData]
+        public void CreateFailureDataOperationResult(string msg, object data)
+        {
+            var result = OperationResult.FailureDataMessage(msg, data);
+            Assert.False(result.IsSuccess);
+            Assert.Equal(msg, result.Message);
+            Assert.Equal(data, result.Data);
+        }
+    }
+}
