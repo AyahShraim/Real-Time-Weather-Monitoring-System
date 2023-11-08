33210e108a724703b404e2c0013ad343ef33c242
Ayah Shraim <ayahshraim558@gmail.com>
2023-11-08 02:07:37 +0200

    update main application Imp

---
+++ RealTimeWeatherMonitoringApp/Utilities/IOutputWriter.cs
@@ -0,0 +1,14 @@
+﻿namespace RealTimeWeatherMonitoringApp.Utilities
+{
+    public interface IOutputWriter
+    {
+        void WriteLine(string message);
+    }
+    public class ConsoleOutputWriter : IOutputWriter
+    {
+        public void WriteLine(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
